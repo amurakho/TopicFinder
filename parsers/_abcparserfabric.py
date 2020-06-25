088c74d714494c83ac10d90976d2ea6bf91844c8
amurakho <amurakho@gmail.com>
2020-06-25 13:19:53 +0300

    split abcfabric

---
+++ parsers/_abcparserfabric.py
@@ -0,0 +1,91 @@
+import abc
+import requests
+
+from errors import Not200RequesCode
+
+
+class AbstractFactory(abc.ABC):
+
+    @abc.abstractmethod
+    def create_search_parser(self):
+        pass
+
+    @abc.abstractmethod
+    def create_top_parser(self):
+        pass
+
+
+class AbstractParser(abc.ABC):
+    """
+    Interface for "search" parsers.
+
+    """
+    keyword = ''
+
+    def __init__(self, is_paginate=False, max_depth=10):
+        self.is_paginate = is_paginate
+        self.max_depth = max_depth
+
+    @abc.abstractmethod
+    def _create_request(self):
+        """
+        Accepts keyword and create request and prepare it for session
+
+        :param keyword:
+        :return: prepared request
+        """
+        pass
+
+    @abc.abstractmethod
+    def parse_content(self, content, depth=0):
+        """
+        Parse content
+
+        :param content:
+        :return:
+        """
+        pass
+
+    @abc.abstractmethod
+    def _pagination(self):
+        """
+        Pagination
+
+        :return:
+        """
+        pass
+
+    def pass_request(self, request: requests.PreparedRequest) -> bytes:
+        """
+        Pass reqeust
+        (if need - pagination)
+
+        :param request: Prepared request
+        :return:
+        """
+        session = requests.Session()
+        response = session.send(request)
+        if response.status_code != 200:
+            raise Not200RequesCode('Error with search request.'
+                                   'Current code is: ', response.status_code)
+        return response.content
+
+    def start_parse(self, keyword: str):
+        """
+        Manage
+
+        1. Make keyword like obj attr
+        2. For keyword create request
+        3. Pass request
+        4. Parse it
+        :return:
+        """
+        self.keyword = keyword
+
+        request = self._create_request()
+
+        content = self.pass_request(request)
+
+        data = self.parse_content(content)
+
+        return data
--- parsers/dwparser.py
+++ parsers/dwparser.py
@@ -3,99 +3,26 @@ import abc
 from bs4 import BeautifulSoup
 
 from errors import Not200RequesCode
-from parsers._abcparser import Parser
+from parsers._abcparserfabric import AbstractFactory, AbstractParser
 
 
-class DwParserTop(Parser):
+class DwParserTop(AbstractParser):
 
-    def _create_request(self, keyword: str) -> requests.PreparedRequest:
+    def _pagination(self) -> str:
+        pass
+
+    def _create_request(self) -> requests.PreparedRequest:
         pass
 
     def parse_content(self, content: str) -> dict:
         pass
 
-    def start_parse(self) -> dict:
-        pass
 
-
-class AbstractSearchParser(abc.ABC):
+class DwParserSearch(AbstractParser):
     """
-    Interface for "search" parsers.
 
+    NOTE: Go to AbstractSearchParser to see more code
     """
-    keyword = ''
-
-    def __init__(self, is_paginate=False, max_depth=10):
-        self.is_paginate = is_paginate
-        self.max_depth = max_depth
-
-
-    @abc.abstractmethod
-    def _create_request(self):
-        """
-        Accepts keyword and create request and prepare it for session
-
-        :param keyword:
-        :return: prepared request
-        """
-        pass
-
-    @abc.abstractmethod
-    def parse_content(self, content, depth=0):
-        """
-        Parse content
-
-        :param content:
-        :return:
-        """
-        pass
-
-    @abc.abstractmethod
-    def _pagination(self):
-        """
-        Pagination
-
-        :return:
-        """
-        pass
-
-    def pass_request(self, request: requests.PreparedRequest) -> bytes:
-        """
-        Pass reqeust
-        (if need - pagination)
-
-        :param request: Prepared request
-        :return:
-        """
-        session = requests.Session()
-        response = session.send(request)
-        if response.status_code != 200:
-            raise Not200RequesCode('Error with search request.'
-                                   'Current code is: ', response.status_code)
-        return response.content
-
-    def start_parse(self, keyword: str):
-        """
-        Manage
-
-        1. Make keyword like obj attr
-        2. For keyword create request
-        3. Pass request
-        4. Parse it
-        :return:
-        """
-        self.keyword = keyword
-
-        request = self._create_request()
-
-        content = self.pass_request(request)
-
-        data = self.parse_content(content)
-
-        return data
-
-
-class DwParserSearch(AbstractSearchParser):
 
     url = 'https://www.dw.com/research/?languageCode=en&item={keyword}' \
           '&searchNavigationId=9097-1452-32771-2469-30687-101618-8120&sort=DATE&' \
@@ -131,31 +58,14 @@ class DwParserSearch(AbstractSearchParser):
         return data
 
 
-class AbstractFactory(abc.ABC):
-
-    @abc.abstractmethod
-    def create_search_parser(self):
-        pass
-
-    @abc.abstractmethod
-    def create_top_parser(self):
-        pass
-
-
 class DwParsersFactory(AbstractFactory):
 
-    def create_search_parser(self) -> AbstractSearchParser:
+    def create_search_parser(self) -> AbstractParser:
         return DwParserSearch()
 
-    def create_top_parser(self):
+    def create_top_parser(self) -> AbstractParser:
         return DwParserTop()
 
 
 # class BbcParsersFactory(AbstractFactory):
 #     pass
-
-
-
-
-#https://www.dw.com/search/en?searchNavigationId=9097&languageCode=en&origin=gN&item=nazi
-#https://www.dw.com/search/en?languageCode=en&origin=gN&item=asd\
