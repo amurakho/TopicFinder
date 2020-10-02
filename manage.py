0d85921d0f291cf7de492084cbd8b364a6e4cdd5
amurakho <amurakho@gmail.com>
2020-10-03 00:34:44 +0300

    add file choices

--- manage.py
+++ manage.py
@@ -5,63 +5,23 @@ import argparse
 
 import const
 import errors
+from parser_runner import get_all_parsers_modules
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 class Commands:
 
-    # def _parse_arguments(self, args, flags, parsed_args=None):
-    #     arg = next(args)
-    #
-    #     if not parsed_args:
-    #         parsed_args = list()
-    #
-    #     if arg.startswith('-') and arg not in flags.keys():
-    #         print(f'Wrong "{arg}" flag with run command')
-    #         return
-    #
-    #     elif arg.startswith('-') and arg in flags.keys():
-    #         is_additional_args = flags[arg]['additional_args']
-    #         parsed_args.append({
-    #             'name': arg,
-    #             'additional_args': [],
-    #         })
-    #
-    #         if is_additional_args:
-    #
-    #             arg = next(args)
-    #             while not arg.startswith('-'):
-    #                 parsed_args[arg]['additional_args'].append(arg)
-    #                 try:
-    #                     arg.
-    #     if args:
-
     def parse_arguments(self, flags):
         parser = argparse.ArgumentParser(prog='run', description='"run" command parse')
         parser.add_argument('run')
-        parser.add_argument('-x', nargs='?')
+        for flag, data in flags.items():
+            action = 'append' if data['additional_args'] else 'store_true'
+            parser.add_argument(flag, action=action)
         print(parser.parse_args())
         # args = sys.argv[2:]
         # flag_names = flags.keys()
 
-    # def _parse_arguments2(self, flags):
-    #     parsed_args = []
-    #
-    #     for arg in args:
-    #         if arg.startswith('-') and arg not in flag_names:
-    #             print(f'Wrong "{arg}" flag with run command')
-    #
-    #         elif arg.startswith('-') and arg in flag_names:
-    #             is_additional_args = flags[arg]['additional_args']
-    #
-    #             add_list = []
-    #             if is_additional_args:
-    #
-    #             d = {
-    #                 'name': arg,
-    #
-    #             }
 
     def test(self):
         suite = unittest.TestLoader().discover(str(const.TEST_FOLDER_PATH))
@@ -74,18 +34,15 @@ class Commands:
                 'additional_args': False,
             },
             '-o': {
-                'name': '-o',
                 'description': 'Launch scrappers by name',
                 'additional_args': True,
+                'choices': [get_all_parsers_modules()]
             },
             '-k': {
-                'name': '-k',
                 'description': 'Launch with keywords',
                 'additional_args': True
             },
         }
-
-
 
         self.parse_arguments(flags)
 
